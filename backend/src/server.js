30861b996b154aa6678a4e126b0738059244f793
Theodore <ijomatheodore@gmail.com>
2026-03-19 18:42:27 +0100

    MongoDb connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
 import epress from "express";
 import { ENV } from "./config/env.js";
-
+import { connectDB } from "./config/db.js";
 
 
 const app = epress();
+connectDB()
 
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on port ${ENV.PORT}`);
