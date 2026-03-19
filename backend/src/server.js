dc329577dc49f88e5fcffd2707bbd5d17eea457a
Theodore <ijomatheodore@gmail.com>
2026-03-19 19:28:13 +0100

    Added code rabbit and refactored server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,23 @@ import epress from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
-
 const app = epress();
-connectDB()
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server is running on port ${ENV.PORT}`);
+app.get("/", (req, res) => {
+  res.send("Hello from the backend!");
 });
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log(`Server is running on port ${ENV.PORT}`);
+      
+    });
+  } catch (error) {
+    console.log("Failed to start server", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
