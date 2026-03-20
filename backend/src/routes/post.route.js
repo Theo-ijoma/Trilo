d49b3eccf5130f22ac1bdd6f82ee6f290faad6f2
Theodore <ijomatheodore@gmail.com>
2026-03-20 15:28:18 +0100

    Post creation api completed

--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -5,6 +5,7 @@ import {
   getUserPosts,
   createPost,
   likePost,
+  deletePost,
 } from "../controllers/post.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import  upload  from "../middleware/upload.middleware.js";
@@ -19,5 +20,6 @@ router.get("/user/:username", getUserPosts);
 // protected routes
 router.post("/", protectRoute, upload.single("image"), createPost);
 router.post("/:postId/like", protectRoute, likePost);
+router.delete("/:postId", protectRoute, deletePost);
 
 export default router;
